1bb571d7d699501ecd82ddea5f77e99e340266a2
PrasastyDaraDDS <daraprasasty075@gmail.com>
2024-12-15 15:37:08 +0700

    add migrations

---
+++ database/migrations/2024_12_15_075225_create_payments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->dateTime('payment_date');
+            $table->decimal('amount', 10, 2);
+            $table->decimal('amount_shipping', 10, 2)->nullable();
+            $table->decimal('amount_overhead', 10, 2)->nullable();
+            $table->timestamps();
+
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
