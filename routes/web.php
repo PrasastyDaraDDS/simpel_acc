77943481fc327f557316ea9cb11509f666e74444
PrasastyDaraDDS <daraprasasty075@gmail.com>
2024-12-22 17:31:49 +0700

    add getter data api

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\OrderStatusController;
 use App\Http\Controllers\ParticipantController;
@@ -9,6 +10,7 @@ use App\Http\Controllers\ProductCategoryController;
 use App\Http\Controllers\ProductCategoryTypeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ResellerOrderController;
+use App\Http\Controllers\StoreController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 /*
@@ -29,16 +31,23 @@ Route::get('index/{locale}', [App\Http\Controllers\HomeController::class, 'lang'
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'root'])->name('root');
 
-Route::resource('participants', ParticipantController::class);
-Route::resource('participant_roles', ParticipantRoleController::class);
-Route::resource('products', ProductController::class);
-Route::resource('orders', OrderController::class);
-Route::resource('payments', PaymentController::class);
-Route::resource('reseller_orders', ResellerOrderController::class);
-Route::resource('product_categories', ProductCategoryController::class);
+Route::middleware(['auth'])->group(function () {
+    Route::resource('customers', CustomerController::class);
+        Route::resource('stores', StoreController::class);
+        Route::resource('participants', ParticipantController::class);
+        Route::resource('participant_roles', ParticipantRoleController::class);
+        Route::resource('products', ProductController::class);
+        Route::resource('orders', OrderController::class);
+        Route::resource('payments', PaymentController::class);
+        Route::resource('reseller_orders', ResellerOrderController::class);
+        Route::resource('product_categories', ProductCategoryController::class);
+        Route::resource('product_category_types', ProductCategoryTypeController::class);
+        Route::resource('order_statuses', OrderStatusController::class);
+});
+
 Route::get('api/product_category_types/{category}', [ProductCategoryTypeController::class, 'showType']);
-Route::resource('product_category_types', ProductCategoryTypeController::class);
-Route::resource('order_statuses', OrderStatusController::class);
+Route::get('api/product/{id}', [ProductController::class, 'getProductById']);
+Route::get('api/order_statuses/{enum_type}', [OrderStatusController::class, 'getOrderStatus']);
 //Update User Details
 Route::post('/update-profile/{id}', [App\Http\Controllers\HomeController::class, 'updateProfile'])->name('updateProfile');
 Route::post('/update-password/{id}', [App\Http\Controllers\HomeController::class, 'updatePassword'])->name('updatePassword');
